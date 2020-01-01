4e195d81ec34f6d14a23a0ffa2721dceacb0bef6
Danh Thanh Nguyen <motsis666@yahoo.com>
2020-01-01 21:23:14 +0700

    dev-home

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Using "az login" to login Azure Subscription.
 # Provider
- #provider "azurerm" { 
+ #Provider "azurerm" { 
     #subscription_id = "54d87296-b91a-47cd-93dd-955bd57b3e9a"
     #tenant_id       = "7d37f2bd-a1dc-4e2c-aaa3-c758dc77fff7"
     #}
