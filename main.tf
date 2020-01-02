e08bb830b93329bc7e8132db18fd4e66ebd671e6
Danh Thanh Nguyen <motsis666@yahoo.com>
2020-01-02 21:58:46 +0700

    dev-home

--- main.tf
+++ main.tf
@@ -1,144 +1,45 @@
-# Using "az login" to login Azure Subscription.
-# Provider
- #Provider "azurerm" { 
-    #subscription_id = "54d87296-b91a-47cd-93dd-955bd57b3e9a"
-    #tenant_id       = "7d37f2bd-a1dc-4e2c-aaa3-c758dc77fff7"
-    #}
+// Create Resource Group
 
-#Create virtual network
-resource "azurerm_virtual_network" "marcelnguyennetwork" {
-    name                = "myVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = var.location
-    resource_group_name = var.resource_group_name
+resource "azurerm_resource_group" "rg_network" {
 
-    tags = {
-        environment = "Marcel Nguyen Terraform Demo"
-    }
+name = "${var.rg_network}"
+
+location = "${var.location}"
+
 }
 
-resource "azurerm_subnet" "marcelnguyensubnet" {
-    name                 = "mySubnet"
-    resource_group_name  = var.resource_group_name
-    virtual_network_name = azurerm_virtual_network.marcelnguyennetwork.name
-    address_prefix       = "10.0.2.0/24"
+// Create Network
+
+module "create_network" {
+
+source = "./modules/1-network"
+
+location = "${azurerm_resource_group.rg_network.location}"
+
+rg_network = "${azurerm_resource_group.rg_network.name}"
+
 }
 
-#Create public IP address
-resource "azurerm_public_ip" "marcelnguyenpublicip" {
-    name                         = "myPublicIP"
-    location                     = var.location
-    resource_group_name          = var.resource_group_name
-    allocation_method            = "Dynamic"
+// Create Windows VM
 
-    tags = {
-        environment = "Marcel Nguyen Terraform Demo"
-    }
-}
+module "windows_vm" {
 
-#Create Network Security Group
-resource "azurerm_network_security_group" "marcelnguyenNetworkSecurityGroup" {
-    name                = "marcelnguyenNetworkSecurityGroup"
-    location            = var.location
-    resource_group_name = var.resource_group_name
-    
-    security_rule {
-        name                       = "RDP"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "3389"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
+source = "./modules/2-windows_vm"
 
-    tags = {
-        environment = "Marcel Nguyen Terraform Demo"
-    }
-}
+computer_name_Windows = "${var.computer_name_Windows}"
 
-#Create virtual network interface card
-resource "azurerm_network_interface" "marcelnguyennic" {
-    name                        = "myNIC"
-    location                    = var.location
-    resource_group_name         = var.resource_group_name
-    network_security_group_id   = azurerm_network_security_group.marcelnguyenNetworkSecurityGroup.id
+rg_network = "${azurerm_resource_group.rg_network.name}"
 
-    ip_configuration {
-        name                          = "myNicConfiguration"
-        subnet_id                     = "${azurerm_subnet.marcelnguyensubnet.id}"
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.marcelnguyenpublicip.id}"
-    }
+subnet_id = "${module.create_network.mgmt_sub_id}"
 
-    tags = {
-        environment = "Marcel Nguyen Terraform Demo"
-    }
-}
+location = "${azurerm_resource_group.rg_network.location}"
 
-#Create storage account for diagnostics
-resource "random_id" "randomId" {
-    keepers = {
-        #Generate a new ID only when a new resource group is defined
-        resource_group = "RG_marcelnguyen_20191227"
-    }
+vmsize = "${var.vmsize}"
 
-    byte_length = 8
-}
+os_ms = "${var.os_ms}"
 
-resource "azurerm_storage_account" "mystorageaccount" {
-    name                        = "diag${random_id.randomId.hex}"
-    resource_group_name         = var.resource_group_name
-    location                    = var.location
-    account_replication_type    = "LRS"
-    account_tier                = "Standard"
+admin_username = "${var.admin_username}"
 
-    tags = {
-        environment = "Marcel Nguyen Terraform Demo"
-    }
-}
+admin_password = "${var.admin_password}"
 
-#Create virtual machine
-resource "azurerm_virtual_machine" "marcelnguyenvm" {
-    name                  = "myVM_marcel.nguyen"
-    location              = var.location
-    resource_group_name   = var.resource_group_name
-    network_interface_ids = [azurerm_network_interface.marcelnguyennic.id]
-    vm_size               = "Standard_DS1_v2"
-
-    os_profile_windows_config {
-        provision_vm_agent=true
-        timezone="Romance Standard Time"
-    }
-    storage_os_disk {
-        name              = "myOsDisk"
-        caching           = "ReadWrite"
-        create_option     = "FromImage"
-        managed_disk_type = "Standard_LRS"
-        os_type = "Windows"
-    }
-
-    storage_image_reference {
-        publisher = "MicrosoftWindowsServer"
-        offer     = "WindowsServer"
-        sku       = "2019-Datacenter"
-        version   = "latest"
-    }
-
-    os_profile {
-        computer_name  = "myvm"
-        admin_username = "marcelnguyen"
-        admin_password = "Thu234567"
-    }
-
-    boot_diagnostics {
-        enabled     = "true"
-        storage_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
-    }
-
-    tags = {
-        environment = "Marcel Nguyen Terraform Demo"
-    }
 }
